e96615db97f6004a53c3b18bd52bda3a93b40733
slicepizza <tmdgns8399@naver.com>
2024-02-29 01:29:54 +0900

    구직페이지 완성, 구인페이지 매칭 기능 구현

---
+++ Elderly_Care/src/main/java/kr/spring/care/matching/service/SeniorService.java
@@ -0,0 +1,41 @@
+package kr.spring.care.matching.service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import kr.spring.care.mockdata.entity.Senior;
+import kr.spring.care.mockdata.entity.User;
+import kr.spring.care.mockdata.repository.SeniorRepository;
+import kr.spring.care.mockdata.repository.UserRepository;
+
+public class SeniorService {
+    private final SeniorRepository seniorRepository;
+    private final UserRepository userRepository;
+
+    public SeniorService(SeniorRepository seniorRepository, UserRepository userRepository) {
+        this.seniorRepository = seniorRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Senior getOrCreateSenior(Long userId, String health, String requirements, Boolean hasGuardian) {
+        Optional<User> userOpt = userRepository.findById(userId);
+        if (!userOpt.isPresent()) {
+            throw new NoSuchElementException("사용자를 찾을 수 없습니다: " + userId);
+        }
+        User user = userOpt.get();
+
+        return seniorRepository.findByUser(user)
+                .orElseGet(() -> createSenior(user, health, requirements, hasGuardian));
+    }
+
+    private Senior createSenior(User user, String health, String requirements, Boolean hasGuardian) {
+        Senior senior = new Senior();
+        senior.setUser(user);
+        senior.setHealth(health);
+        senior.setRequirements(requirements);
+        senior.setHasGuardian(hasGuardian);
+        return seniorRepository.save(senior);
+    }
+
+
+}

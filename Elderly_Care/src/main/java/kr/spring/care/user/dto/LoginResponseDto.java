6341aefc87cbec082b0e02ebdeac248da60204fa
slicepizza <tmdgns8399@naver.com>
2024-04-04 11:01:50 +0900

    LoginResponseDto를 lombok 어노테이션으로 간소화

--- Elderly_Care/src/main/java/kr/spring/care/user/dto/LoginResponseDto.java
+++ Elderly_Care/src/main/java/kr/spring/care/user/dto/LoginResponseDto.java
@@ -1,29 +1,14 @@
 package kr.spring.care.user.dto;
 
 import kr.spring.care.user.constant.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class LoginResponseDto {
     private String email;
     private Role role;
-    
-    public LoginResponseDto(String email, Role role) {
-        this.email = email;
-        this.setRole(role);
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-	public Role getRole() {
-		return role;
-	}
-
-	public void setRole(Role role) {
-		this.role = role;
-	}
 }
